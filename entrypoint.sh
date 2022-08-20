d463ac02f7df7604603c80f87abda1c3bb5c701f
hansjoergventx <hans-joerg@ventx.de>
2022-08-20 22:49:56 +0200

    feature: switch to directory (which seems to be more stable working thant rover workingdir oder planfile arguments)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
-rover -planPath "$1" -standalone true
-rover -planPath "$1" -genImage true
-rsvg-convert -h 1080 rover.svg -o "$2"
+cd "$1" || exit
+rover -planPath "$2" -standalone true
+rover -planPath "$2" -genImage true
+rsvg-convert -h 1080 rover.svg -o "$3"
