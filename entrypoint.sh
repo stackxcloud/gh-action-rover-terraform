59c04dbb7761134d3f53aec00f06035d8775afe7
hansjoergventx <hans-joerg@ventx.de>
2022-08-20 22:34:37 +0200

    feature: :switch to 'planPath' so we do not need to convert terraform plan output to JSON

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
-rover -planJSONPath "$1" -standalone true
-rover -planJSONPath "$1" -genImage true
+rover -planPath "$1" -standalone true
+rover -planPath "$1" -genImage true
 rsvg-convert -h 1080 rover.svg -o "$2"
