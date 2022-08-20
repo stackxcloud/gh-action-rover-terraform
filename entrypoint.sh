d2adfe062567f25c1702086c8bcd08f12a0ee08f
hansjoergventx <hans-joerg@ventx.de>
2022-08-21 00:08:54 +0200

    feature: switch to default to use current (root) directory of repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
-cd "$1" || exit
+
+if [ "$1" != "." ]; then
+  echo "Changing directory to: $1"
+  cd "$1" || exit 1
+fi
+
 rover -planPath "$2" -standalone true
 rover -planPath "$2" -genImage true
 rsvg-convert -h "$4" rover.svg -o "$3"
