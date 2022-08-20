78a4c2e6b32531b98ff1a1fae687d159edc259c6
hansjoergventx <hans-joerg@ventx.de>
2022-08-20 19:27:07 +0200

    feature: switched from 'workingDir' (which requires rover initiated terraform init) to 'planJSONPath', which just expects a plan.json file as input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
-rover -workingDir "$1" -standalone true
-rover -workingDir "$1" -genImage true
+rover -planJSONPath "$1" -standalone true
+rover -planJSONPath "$1" -genImage true
 rsvg-convert -h 1080 rover.svg -o "$2"
